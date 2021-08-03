9d7db676934d4b1d56a561248fe683ddee616bd7
Yura Mitulya <yuramitulya@gmail.com>
2021-08-03 19:57:55 +0300

    init

---
+++ Block/Index.php
@@ -0,0 +1,10 @@
+<?php
+declare (strict_types=1);
+namespace Perspective\LayoutExp\Block;
+class Index extends \Magento\Framework\View\Element\Template
+{
+    public function getSubTitle()
+    {
+        return 'Learn Magento 2 Block Layout';
+    }
+}
